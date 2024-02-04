d3b29994c3791581b296f4018fe0ed1e6aa58345
Jabko <jabko@Development-Air.fritz.box>
2024-02-04 17:43:35 +0100

    feat: Implement Dagger-Hilt and its module, update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,6 @@ dependencies {
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
     //Dagger - Hilt
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
 }
--- app/src/main/java/com/example/to_docompose/utils/Constants.kt
+++ app/src/main/java/com/example/to_docompose/utils/Constants.kt
@@ -3,5 +3,5 @@ package com.example.to_docompose.utils
 object Constants {
 
     const val DATABASE_TABLE_NAME = "todo_table"
-
+    const val DATABASE_NAME = "todo_database"
 }
