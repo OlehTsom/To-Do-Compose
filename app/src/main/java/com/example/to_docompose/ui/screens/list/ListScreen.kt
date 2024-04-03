ef37ae75d84dc23d10232993b78657a71aad6295
Jabko <golohilen@gmail.com>
2024-04-03 22:37:50 +0200

    feat: Added basic SnackBar functionality

--- app/src/main/java/com/example/to_docompose/ui/screens/list/ListScreen.kt
+++ app/src/main/java/com/example/to_docompose/ui/screens/list/ListScreen.kt
@@ -2,7 +2,6 @@ package com.example.to_docompose.ui.screens.list
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -10,11 +9,15 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
@@ -22,7 +25,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.to_docompose.R
 import com.example.to_docompose.ui.viewmodels.SharedVieModel
+import com.example.to_docompose.utils.Action
 import com.example.to_docompose.utils.SearchAppBarState
+import kotlinx.coroutines.launch
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -48,10 +53,21 @@ fun ListScreen(
 
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
-    sharedVieModel.handleDatabaseActions(action)
+
+    val snackBarHostState = remember{SnackbarHostState()}
+
+    DisplaySnackBar(
+        snackBarHostState = snackBarHostState,
+        handleDatabaseActions = { sharedVieModel.handleDatabaseActions(action) },
+        taskTitle = sharedVieModel.title.value,
+        action = action
+    )
 
     Scaffold(modifier = Modifier
         .nestedScroll(scrollBehavior.nestedScrollConnection),
+        snackbarHost = {
+            SnackbarHost(hostState = snackBarHostState)
+        },
         topBar = {
            ListAppTopBar(
                sharedVieModel = sharedVieModel,
@@ -93,6 +109,33 @@ fun ListFab(
             contentDescription = stringResource(R.string.add_button)
         )
     }
+}
+
+
+@Composable
+fun DisplaySnackBar(
+    snackBarHostState: SnackbarHostState,
+    handleDatabaseActions: () -> Unit,
+    taskTitle: String,
+    action: Action
+){
+
+    handleDatabaseActions()
+
+    val scope = rememberCoroutineScope()
+
+    LaunchedEffect(key1 = action){
+        if (action != Action.NO_ACTION){
+            scope.launch {
+                val snackBarResult = snackBarHostState
+                    .showSnackbar(
+                        message = "${action.name}: $taskTitle",
+                        actionLabel = "Prima"
+                    )
+            }
+        }
+    }
+
 }
 
 
